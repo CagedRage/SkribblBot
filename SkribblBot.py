aa42ae3a9348522290fb31014ea1490bd68007c4
Jung Choi <jungchoi2023@gmail.com>
2020-06-12 15:14:55 -0700

    IT HAS COLOR NOW WOOOOOT

--- SkribblBot.py
+++ SkribblBot.py
@@ -3,6 +3,8 @@ import sys
 import pyautogui
 from time import sleep
 import numpy as np
+
+
 # from webcolors import rgb_to_name
 # import webcolors
 # from colour import Color
@@ -22,34 +24,77 @@ def matprint(mat, fmt="g"):
 def drawpictureasarray(image):
     width, height = image.size  # getting width and height allows us to iterate over each pixel
     imageasarray = np.zeros((height // 10, width // 10))
-    for x in range(0, width):
-        for y in range(0, height):
+    for x in range(0, width, 10):
+        for y in range(0, height, 10):
+            if x == 150 and y == 150:
+                print("owo?")
             r, g, b = image.getpixel((x, y))
-            if not (r == 255 and b == 255 and g == 255):
-                imageasarray[y // 10 - 1, x // 10 - 1] = 1.0
+            imageasarray[y // 10 - 1, x // 10 - 1] = getcolorrgb([r, g, b])
 
     return imageasarray
 
 
-# Make numpy not truncate when printing out arrays to display
-np.set_printoptions(threshold=sys.maxsize)
+# FORMAT: index is color code, 2d list of R G B respectively
+def recordskribblpalette():  # records the RGB values of the skribbl color palette using color codes in getcolorrgb
+    return [[255, 255, 255], [193, 193, 193], [239, 19, 11], [255, 113, 0], [255, 228, 0], [0, 204, 0], [0, 178, 255],
+            [35, 31, 211], [163, 0, 186], [211, 124, 170], [160, 82, 45],  # end of row 1
+            [0, 0, 0], [76, 76, 76], [116, 11, 7], [194, 56, 0], [232, 162, 0], [0, 85, 16], [0, 86, 158], [14, 8, 101],
+            [85, 0, 105], [167, 85, 116], [99, 48, 13]]  # end of row 2
+    pass
 
-# Open the image - image is now variable skribblImg
+
+# "color" parameter is [r, g, b]
+# returns color code with least distance
+def getcolorrgb(color):  # "Rounds" to nearest color in skribbl based on RGB values using simpler form of 3d distance
+    global skribblcolorpalette  # used from recordskribblpalette function
+    # r, g, b arguments should be color of pixel in image
+    # skribbl color palette https://cdn.discordapp.com/attachments/674087507260080142/721115804917301249/unknown.png
+
+    # the color codes are, in the same grid,
+    # 0  1  2  3  4  5  6  7  8  9  10
+    # 11 12 13 14 15 16 17 18 19 20 21
+
+    # closestcolor stores the closest color in skribbl palette to the color argument
+    # index 0 is the "distance", index 1 is the index (color code)
+    # default to distance 99999 so it hopefully doesn't stick with r=69, g=69, b=69
+    closestcolor = [99999, -1]
+
+    for x in range(len(skribblcolorpalette)):
+        # for each color in skribbl's color palette
+        # if the distance between current color and color in image is less than the previous closest color's distance
+        # then store the current color as the new closest color
+        # and new least distance as current distance
+        # and color code as current color code
+        dist = getdistance(color, skribblcolorpalette[x])
+        if dist < closestcolor[0]:
+            closestcolor[0] = dist
+            closestcolor[1] = x
+
+    return closestcolor[1]
+
+
+# gets the distance between two colors, [r, g, b] and [r, g, b]
+# it's just the sum of (r-r2)^2, (g-g2)^2, and (b-b2)^2
+def getdistance(color, color2):  # Finds the 3d distance between two colors: [r, g, b] and [r, g, b]
+    # _dist is "_ distance" in english
+    rdist = abs(color[0] - color2[0])
+    gdist = abs(color[1] - color2[1])
+    bdist = abs(color[2] - color2[2])
+
+    return rdist + gdist + bdist
+
+
+skribblcolorpalette = recordskribblpalette()  # Store skribbl colors RGB values using the color codes in getcolorrgb
+np.set_printoptions(threshold=sys.maxsize)  # Make numpy not truncate when printing out arrays to display
 try:
-    skribblImg = Image.open("skribbl.jpg")  # image in same folder as py file
+    skribblImg = Image.open("skribbl.jpg")  # image in same folder as py file is now stored as skribblImg
 except IOError:
     print("bruh ur image is bad like u")  # error called if image is not found or something else
     sys.exit(1)
-
 # skribblImg.show() # uncomment this if you want to display the image
-
-imagepixel = drawpictureasarray(skribblImg)
-
-matprint(imagepixel)  # aaaa
-
-# selected_color variable keeps track of currently selected color
-selected_color = "black"
-
+imagepixel = drawpictureasarray(skribblImg)  # Generate the 2d ndarray for the skribblImg
+matprint(imagepixel)  # Print the array
+selected_color = "black"  # selected_color variable keeps track of currently selected color
 pyautogui.FAILSAFE = False  # hope this doesnt brick my computer
 
 """
@@ -109,4 +154,3 @@ def closest_colour(requested_colour):
 # pyautogui.moveTo(586, 953, 1)
 
 # Point(x=485, y=300) FOR SKRIBBL TOP LEFT
-
